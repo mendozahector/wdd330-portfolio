da3bf2d1665e48433309faea95ad998852f10f2d
Hector Mendoza <hectormendoza@byui.edu>
2022-10-22 23:25:14 -0400

    final css touches

--- projects/todo/script.js
+++ projects/todo/script.js
@@ -1,59 +1,83 @@
 
 // [{ id : timestamp, content: string, completed: bool } ]
-let todoItems = []
-let activeItems = "All"
+let todoItems = [];
+let activeItems = "All";
 
 function show(type) {
-    activeItems = type
+    activeItems = type;
+    updateUI();
 }
 
 function deleteTask(i) {
     todoItems.splice(i,1);
-    updateUI();
+}
+
+function checkTask(i) {
+    todoItems[i]["completed"] = !todoItems[i]["completed"];
 }
 
 function updateUI() {
+    todoListHtml = document.getElementById("todo-list");
+    todoListHtml.textContent = "";
+    let i = 0;
+    todoItems.forEach(item => {
+        if (activeItems == "All" ||
+           (activeItems == "Active" && item["completed"] == false) ||
+           (activeItems == "Completed" && item["completed"] == true)) {
+            const li = document.createElement("li");
+            li.innerHTML = item["content"];
+            li.value = i;
+            if (item["completed"] == true) {
+                li.classList.add("completed");
+            }
+            const b = document.createElement("button");
+            b.innerHTML = "&#10060";
+            b.value = i;
+            li.appendChild(b);
+            todoListHtml.appendChild(li);
+        }
+        i += 1;
+    });
 
-    const totalTasks = todoItems.length;
-    const toolButtons = {"all-button": "All", "active-button": "Active", "completed-button": "Completed"}
+    const totalTasks = todoListHtml.childElementCount;
+    const toolButtons = {"all-button": "All", "active-button": "Active", "completed-button": "Completed"};
 
     if (totalTasks >= 1) {
-        document.getElementById("total-tasks").innerHTML = totalTasks == 1 ? "1 task left" : `${totalTasks} tasks left`;
+        let message = "";
+        if (activeItems == "Completed") {
+            message = totalTasks == 1 ? "1 task completed" : `${totalTasks} tasks completed`;
+        } else {
+            message = totalTasks == 1 ? "1 task left" : `${totalTasks} tasks left`;
+        }
+
+        document.getElementById("total-tasks").innerHTML = message;
         
         const todoTools = document.getElementById("todo-tools");
         if (todoTools.childElementCount <= 1) {
             for (const [key, value] of Object.entries(toolButtons)) {
                 const button = document.createElement("button");
-                button.id = key
-                button.innerHTML = value
-                // button.addEventListener('click', function(){
-                //     show(value);
-                // });
+                button.id = key;
+                button.classList.add("tool-button");
+                button.innerHTML = value;
+                button.addEventListener('click', function(){
+                    show(value);
+                });
                 todoTools.appendChild(button);
               }
         }
         
     } else {
-        for (const [key, _] of Object.entries(toolButtons)) {
-            document.getElementById(key).remove();
-          }
-        document.getElementById("total-tasks").innerHTML = "Please add a task &#128071 &#128221 &#128526";
+        if (activeItems == "All" || todoItems.length < 1) {
+            for (const [key, _] of Object.entries(toolButtons)) {
+                document.getElementById(key).remove();
+              }
+            document.getElementById("total-tasks").innerHTML = "Please add a task &#128071 &#128221 &#128526";
+        } else {
+            const message = activeItems == "Active" ? "No active tasks &#128526" : "No completed tasks! Get to work! &#128170";
+            document.getElementById("total-tasks").innerHTML = message;
+        }
+        
     }
-
-    todoListHtml = document.getElementById("todo-list");
-    todoListHtml.textContent = "";
-    todoItems.forEach(item => {
-        const i = todoListHtml.childElementCount;
-        const li = document.createElement("li");
-        li.innerHTML = item["content"];
-        const b = document.createElement("button");
-        b.innerHTML = "&#10060";
-        b.addEventListener('click', function(){
-            deleteTask(i);
-        });
-        li.appendChild(b);
-        todoListHtml.appendChild(li);
-    });
 }
 
 function addItem() {
@@ -69,3 +93,25 @@ function addItem() {
 }
 
 updateUI();
+
+const taskInputHtml = document.getElementById("todo-item");
+taskInputHtml.addEventListener("keypress",function (event) {
+    if (event.key == "Enter") {
+        event.preventDefault();
+        document.getElementById("add-task").click();
+    }
+});
+
+const list = document.querySelector("ol");
+list.addEventListener("click", function(ev) {
+    const tagName = ev.target.tagName;
+    const i = ev.target.value;
+
+    if (tagName == "LI") {
+        checkTask(i);
+    } else if (tagName == "BUTTON") {
+        deleteTask(i);
+    }
+
+    updateUI();
+});
